584f0a9048b6f3744e2a92a07a9d8b6232d00d73
Fatima C Borges <fatimacarvalhob@gmail.com>
2020-01-22 14:05:00 -0300

    ajustes conexão

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-  HOST: "us-cdbr-iron-east-02.cleardb.net",
+/*   HOST: "us-cdbr-iron-east-02.cleardb.net",
   USER: "b7e24378878xxx",
   PASSWORD: "0200exxx",
-  DB: "heroku_7643ec736354xxx"
+  DB: "heroku_7643ec736354xxx" */
+  host: 'us-cdbr-iron-east-05.cleardb.net',
+  user: 'b6e19e15833f09',
+  password: 'fb36c972',
+  database: 'heroku_db6d4b5a5e3e081'
+
 };
